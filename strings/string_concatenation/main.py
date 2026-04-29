7b88e6a6418cbb2c59db67c0214781b5011d2341
vekemanward0-design <>
2026-04-29 11:29:21 +0000

    String Concatenation

---
+++ strings/string_concatenation/main.py
@@ -0,0 +1,13 @@
+string1 = "country: "
+string2 = "Italy"
+string3 = "Ukraine"
+string4 = "Egypt"
+
+# Merge variables to form phrases
+output1 = string1 + string2
+output2 = string1 + string3
+output3 = string1 + string4
+
+print("Variable output1 equals:", f'"{output1}"')
+print("Variable output2 equals:", f'"{output2}"')
+print("Variable output3 equals:", f'"{output3}"')
